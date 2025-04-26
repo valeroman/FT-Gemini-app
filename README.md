32f07642fced511e4ad2ed4dd31822cc221ee21b
valeroman <valeroman@gmail.com>
2025-04-26 09:24:43 -0500

    09-baseUrl-env

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Dev
 
-1. Ejecutar el comando de:
+1. Clonar el archivo `.env.template` y renombrarlo a `.env` y completar los valores necesarios.
+2. Ejecutar el comando de:
 
 ```dart
 dart run build_runner watch
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gemini_app/config/router/app_router.dart';
 import 'package:gemini_app/config/theme/app_theme.dart';
 
-void main() {
+void main() async {
   AppTheme.setSystemUIOverlayStyle(isDarkMode: true);
+  await dotenv.load(fileName: ".env");
+
+  //print(dotenv.env['ENDPOINT_API']);
 
   runApp(ProviderScope(child: GeminiApp()));
 }
