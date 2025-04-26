b825383169cb963cca1217075f2f87991a011363
valeroman <valeroman@gmail.com>
2025-04-26 16:34:08 -0500

    13-peticion-http-retorno-stream

--- lib/config/gemini/gemini_impl.dart
+++ lib/config/gemini/gemini_impl.dart
@@ -16,4 +16,27 @@ class GeminiImpl {
       throw Exception("Can't get Gemini response");
     }
   }
+
+  // Stream
+  Stream<String> getResponseStream(String prompt) async* {
+    // TODO: tener presente que enviaremos imagenes
+    //! Multipart
+
+    final body = jsonEncode({'prompt': prompt});
+    final response = await _http.post(
+      '/basic-prompt-stream',
+      data: body,
+      options: Options(responseType: ResponseType.stream),
+    );
+
+    final stream = response.data.stream as Stream<List<int>>;
+    String buffer = '';
+
+    await for (final chunk in stream) {
+      final chunkString = utf8.decode(chunk, allowMalformed: true);
+      buffer += chunkString;
+
+      yield buffer;
+    }
+  }
 }
