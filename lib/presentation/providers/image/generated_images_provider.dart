d52f84c4e75753c2b954b04a2e10f4be1a6e717e
valeroman <valeroman@gmail.com>
2025-05-13 11:33:31 -0500

    25-proveedor-generador-imagenes

---
+++ lib/presentation/providers/image/generated_images_provider.dart
@@ -0,0 +1,63 @@
+import 'package:gemini_app/config/gemini/gemini_impl.dart';
+import 'package:gemini_app/presentation/providers/image/generated_history_provider.dart';
+import 'package:gemini_app/presentation/providers/image/is_generating_provider.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'generated_images_provider.g.dart';
+
+@riverpod
+class GeneratedImages extends _$GeneratedImages {
+  final GeminiImpl gemini = GeminiImpl();
+
+  late final IsGenerating isGeneratingNotifier;
+  late final GeneratedHistory generatedHistoryNotifier;
+
+  String previousPrompt = '';
+  List<XFile> previousImages = [];
+
+  @override
+  List<String> build() {
+    isGeneratingNotifier = ref.read(isGeneratingProvider.notifier);
+    generatedHistoryNotifier = ref.read(generatedHistoryProvider.notifier);
+    return [];
+  }
+
+  void addImages(String imageUrl) {
+    generatedHistoryNotifier.addImage(imageUrl);
+    state = [...state, imageUrl];
+  }
+
+  void clearImages() {
+    state = [];
+  }
+
+  Future<void> generateImage(
+    String prompt, {
+    List<XFile> images = const [],
+  }) async {
+    isGeneratingNotifier.setIsGenerating();
+
+    final imageUrl = await gemini.generateImage(prompt, files: images);
+
+    if (imageUrl == null) {
+      isGeneratingNotifier.setIsNotGenerating();
+      return;
+    }
+
+    previousPrompt = prompt;
+    previousImages = images;
+
+    addImages(imageUrl);
+
+    if (state.length == 1) {
+      generateImageWithPreviousPrompt();
+    }
+  }
+
+  Future<void> generateImageWithPreviousPrompt() async {
+    if (previousPrompt.isEmpty) return;
+
+    await generateImage(previousPrompt, images: previousImages);
+  }
+}
---
+++ lib/presentation/providers/image/generated_images_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'generated_images_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$generatedImagesHash() => r'fda22e8d463621261964c2e42373312af3962ae5';
+
+/// See also [GeneratedImages].
+@ProviderFor(GeneratedImages)
+final generatedImagesProvider =
+    AutoDisposeNotifierProvider<GeneratedImages, List<String>>.internal(
+      GeneratedImages.new,
+      name: r'generatedImagesProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$generatedImagesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$GeneratedImages = AutoDisposeNotifier<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
